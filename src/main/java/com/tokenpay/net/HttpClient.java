02064eda795c6c6400a33e42458440e49ed46820
Onur Polattimur <onur.polattimur@gmail.com>
2020-11-04 16:43:42 +0300

    update http client timeouts value

--- src/main/java/com/tokenpay/net/HttpClient.java
+++ src/main/java/com/tokenpay/net/HttpClient.java
@@ -22,14 +22,15 @@ public class HttpClient {
     private static final String APPLICATION_JSON = "application/json";
     private static final String CONTENT_TYPE = "Content-Type";
     private static final String ACCEPT = "Accept";
-    private static final int TIMEOUT = 140000;
+    private static final int CONNECT_TIMEOUT = 15000;
+    private static final int READ_TIMEOUT = 140000;
     private static final Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss").create();
 
     private HttpClient() {
     }
 
     public static <T> T get(String url, Class<T> responseType) {
-        return exchange(url, GET, new HashMap<String, String>(), null, responseType);
+        return exchange(url, GET, new HashMap<>(), null, responseType);
     }
 
     public static <T> T get(String url, Map<String, String> headers, Object request, Class<T> responseType) {
@@ -53,21 +54,25 @@ public class HttpClient {
             String body = gson.toJson(request);
             InputStream content = request == null ? null : new ByteArrayInputStream(body.getBytes(DEFAULT_CHARSET));
             String responseBody = send(url, httpMethod, content, headers);
-            Response response = gson.fromJson(responseBody, Response.class);
-            if (response.getErrors() != null) {
-                ErrorResponse errors = response.getErrors();
-                throw new TokenPayException(errors.getErrorCode(), errors.getErrorDescription(), errors.getErrorGroup());
-            }
-            if (responseType == Void.class) {
-                return null;
-            }
-
-            return gson.fromJson(response.getData(), responseType);
+            return handleResponse(responseType, responseBody);
         } catch (TokenPayException e) {
             throw e;
         } catch (Exception e) {
             throw new TokenPayException(e);
         }
+    }
+
+    private static <T> T handleResponse(Class<T> responseType, String responseBody) {
+        Response response = gson.fromJson(responseBody, Response.class);
+        if (response.getErrors() != null) {
+            ErrorResponse errors = response.getErrors();
+            throw new TokenPayException(errors.getErrorCode(), errors.getErrorDescription(), errors.getErrorGroup());
+        }
+        if (responseType == Void.class) {
+            return null;
+        }
+
+        return gson.fromJson(response.getData(), responseType);
     }
 
     private static String send(String url, HttpMethod httpMethod, InputStream content, Map<String, String> headers) throws IOException {
@@ -77,11 +82,9 @@ public class HttpClient {
             raw = new URL(url).openConnection();
             conn = (HttpURLConnection) raw;
 
-//            conn.setSSLSocketFactory(socketFactory);
             conn.setRequestMethod(httpMethod.name());
-
-            conn.setConnectTimeout(TIMEOUT);
-            conn.setReadTimeout(TIMEOUT);
+            conn.setConnectTimeout(CONNECT_TIMEOUT);
+            conn.setReadTimeout(READ_TIMEOUT);
             conn.setUseCaches(false);
             conn.setInstanceFollowRedirects(false);
 
@@ -110,18 +113,14 @@ public class HttpClient {
     private static void prepareRequestBody(HttpMethod httpMethod, InputStream content, HttpURLConnection conn) throws IOException {
         if (HttpMethod.hasBody(httpMethod)) {
             conn.setDoOutput(true);
-            final OutputStream output = conn.getOutputStream();
-            try {
+            try (content; OutputStream output = conn.getOutputStream()) {
                 prepareOutputStream(content, output);
-            } finally {
-                output.close();
-                content.close();
             }
         }
     }
 
     private static void prepareOutputStream(InputStream content, OutputStream output) throws IOException {
-        final byte[] buffer = new byte[8192];
+        final byte[] buffer = new byte[16384];
         for (int bytes = content.read(buffer); bytes != -1;
              bytes = content.read(buffer)) {
             output.write(buffer, 0, bytes);
@@ -141,7 +140,7 @@ public class HttpClient {
         } else {
             try {
 
-                final byte[] buffer = new byte[8192];
+                final byte[] buffer = new byte[16384];
                 final ByteArrayOutputStream output = new ByteArrayOutputStream();
                 for (int bytes = input.read(buffer); bytes != -1;
                      bytes = input.read(buffer)) {
